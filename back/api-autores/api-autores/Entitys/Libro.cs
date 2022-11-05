8b6b56a8e2e49cfcb29509b795a8f3de1633ab79
Cristhian-Yactayo <yactayo5c@gmail.com>
2022-11-05 04:29:42 -0500

    Version Inicial

---
+++ back/api-autores/api-autores/Entitys/Libro.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api_autores.Entitys
+{
+    public class Libro
+    {
+        [Key]
+        public int codigolibro { get; set; }
+        //definimos valores no nulos
+        [Required]
+        //definimos el tamaño del campo
+        [StringLength(
+            maximumLength: 100,
+            ErrorMessage = "Se tiene que ingresar un nombre"
+            )]
+        public string titulo { get; set; }
+        [Required]
+        public bool estado { get; set; }
+        [Required]
+        public Autor autor { get; set; }
+    }
+}
